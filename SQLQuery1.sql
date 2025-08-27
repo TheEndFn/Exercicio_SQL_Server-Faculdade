4e65bb8603f628a18380ecc7f5b77cc8d0753554
Filipe Nóbrega <nuncanem32@gmail.com>
2025-08-27 17:54:57 -0300

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -11,6 +11,16 @@ SELECT nrAgencia, nrConta, vlSaldo FROM CONTA
 WHERE vlSaldo > 100 and vlLimiteChequeEsp < 3000
 
 /* Ecercício 3*/
-SELECT * FROM CLIENTE
-WHERE dsEmail LIKE '%@%'
+SELECT nmCliente, dsEmail FROM CLIENTE
+WHERE dsEmail IS NULL
 
+SELECT nmCliente, dsEmail, dtAberturaConta FROM CLIENTE
+WHERE dsEmail LIKE '%@%' 
+
+/* Exercício 4*/
+SELECT nmCliente, nrCPF FROM CLIENTE
+WHERE nmCliente LIKE 'A%'
+
+/* Exercícios 5*/
+SELECT * FROM TIPO_TRANSACAO, TRANSACAO
+ORDER BY 2,6,7 DESC
