a553c8ae0dde954af54874e5b705c08b6bf60d2d
Filipe Nóbrega <nuncanem32@gmail.com>
2025-08-28 16:35:15 -0300

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,21 +1,19 @@
 USE SistemaBancario
 
 /* Exercícios 1*/
-
 SELECT nrAgencia, nrConta, vlSaldo FROM CONTA
 WHERE vlSaldo > 1000
 ORDER BY 3 DESC
 
 /* Exercícios 2*/
 SELECT nrAgencia, nrConta, vlSaldo FROM CONTA
-WHERE vlSaldo > 100 and vlLimiteChequeEsp < 3000
+WHERE vlSaldo > 100 and vlSaldo < 3000
 
 /* Ecercício 3*/
 SELECT nmCliente, dsEmail FROM CLIENTE
-WHERE dsEmail IS NULL
 
 SELECT nmCliente, dsEmail, dtAberturaConta FROM CLIENTE
-WHERE dsEmail LIKE '%@%' 
+WHERE dsEmail LIKE '%@%'
 
 /* Exercício 4*/
 SELECT nmCliente, nrCPF FROM CLIENTE
